cd5e7b525e47639280b7b8f03cbe4275712fd296
Rishabh John <104306063+RishJohn14@users.noreply.github.com>
2022-11-01 20:28:27 +0800

    LoginStatus.java
    Login Status File with 2 options (Success/Failure)

---
+++ Moblima/src/MOBLIMA/LoginStatus.java
@@ -0,0 +1,18 @@
+
+
+package MOBLIMA
+
+public enum LoginStatus
+{ SUCCESSFUL("Login Successful"),FAILED("Login Failed, please enter valid credentials");
+  private String status;
+  
+  LoginStatus(String status)
+  {
+      this.status=status;
+  }
+  
+  public String returningStatus()
+  {
+     return status;
+  }
+}
