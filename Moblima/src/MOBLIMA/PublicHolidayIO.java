361f7a1f21d8f4392e556a04a610881025b8fe0f
pareenakaur <pareenakaur72@gmail.com>
2022-11-02 21:44:33 +0800

    Add files via upload

---
+++ Moblima/src/MOBLIMA/PublicHolidayIO.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class PublicHolidayIO {
+	
+	public String[] readPublicHolidays() throws IOException,Exception {
+		
+		FileReader text_file = new FileReader("data/PublicHolidays.txt");
+		BufferedReader reader = new BufferedReader(text_file);
+	    		
+		try {
+			return reader.readLine().split("[|]");
+			
+		} catch(Exception error) {
+			
+			String[] str = new String[0];
+			return str;
+		}
+	            	
+	    			
+	}	
+	    
+
+	public void writePublicHoliday(String[] publicHolidays) throws IOException,Exception{
+		
+		try {
+			
+			FileWriter text_file = new FileWriter("data/PublicHolidays.txt", false);
+			BufferedWriter reader = new BufferedWriter(text_file);
+				
+			for(int i=0; i<publicHolidays.length; i++)
+				reader.write(publicHolidays[i] + "|");
+				reader.close();
+								
+		} catch (IOException error) {
+			
+			error.printStackTrace();
+		}
+	    	
+	}
+	    
+
+
+}
