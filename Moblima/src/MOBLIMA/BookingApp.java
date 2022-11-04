ce7bce03cc02357efee12e283f83c4e7cc919eb9
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2022-11-04 13:01:05 +0800

    Update BookingApp.java

--- Moblima/src/MOBLIMA/BookingApp.java
+++ Moblima/src/MOBLIMA/BookingApp.java
@@ -3,49 +3,44 @@ package MOBLIMA;
 import Menu.*;
 import java.util.Stack;
 
-public class BookingApp {	
+public class BookingApp {
 	static Stack<MenuPage> menuStack;
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		menuStack = new Stack<MenuPage>();
 		menuStack.push(new MainMenuPage()); //
 		menuStack.peek().Initialize();
-		
-		while(!menuStack.isEmpty())
-		{
+
+		while (!menuStack.isEmpty()) {
 			MenuPage currentMenu = menuStack.peek();
 			System.out.println("");
 			currentMenu.Update();
-			
+
 			// if goNext Flag is set to true, go to next page
-			if(currentMenu.goNext)
-			{
+			if (currentMenu.goNext) {
 				menuStack.push(currentMenu.getNextPage());
 				menuStack.peek().Initialize();
 			}
-			
+
 			// if endMenu Flag is set to true, pop page from stack.
-			if(currentMenu.endMenu)
-			{
+			if (currentMenu.endMenu) {
 				MenuPage prevMenu = menuStack.pop();
 				prevMenu.End();
-				
+
 				// While stack is not empty, pop page if endWhenGoNext flag is set to true
-				while(!menuStack.isEmpty())
-				{
-					if(menuStack.peek().endWhenGoNext)
+				while (!menuStack.isEmpty()) {
+					if (menuStack.peek().endWhenGoNext)
 						menuStack.pop();
 					// Else, set goNext flag back to false to run page again
-					else
-					{
+					else {
 						menuStack.peek().goNext = false;
 						break;
 					}
 				}
 			}
 		}
-		
+
 		System.out.println("Exiting MOBLIMA...");
 	}
 }
