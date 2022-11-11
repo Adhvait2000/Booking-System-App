a21bc2ff94d4921c07aa47edfecf5231f46c2053
pareenakaur <pareenakaur72@gmail.com>
2022-11-11 20:37:14 +0800

    Created javadoc

--- Moblima/src/MOBLIMA/Cineplex.java
+++ Moblima/src/MOBLIMA/Cineplex.java
@@ -1,47 +1,93 @@
 package MOBLIMA;
 import java.util.*;
 
+/**
+  Create and obtain Cineplexes using relevant information
+  There are three cinemas in each Cineplex
+  @version 1.0
+  @since 2022-11-01
+ */
 public class Cineplex { 
 	
-	private String nameCineplex; //name of cineplex
+	/**
+	 * Name of Cineplex
+	 */
+	private String nameCineplex; 
     
-    private int idCineplex; //cineplex ID
+	/**
+	 * ID of a Cineplex
+	 */
+    	private int idCineplex; 
     
-    private ArrayList<Cinema> listCinemas; //list of cinemas in each cineplex
+	/**
+	 * List of cinemas in each cineplex
+	 */
+  	private ArrayList<Cinema> listCinemas; 
+	
+	/**
+	 * Constructor for Cineplex
+	 * sets the name and id of the cineplex and creates a new list of Cinemas for the Cineplex
+	 * @param nameCineplex Name of this cineplex
+	 * @param idCineplex ID of this Cineplex
+	 */
+	public Cineplex(String nameCineplex, int idCineplex) { 
+        	this.nameCineplex = nameCineplex; 
+        	this.idCineplex = idCineplex; 
+        	this.listCinemas = new ArrayList<Cinema>();
+   	}
 
-    public Cineplex(String nameCineplex, int idCineplex) { //constructor for the class
-        this.nameCineplex = nameCineplex; //name of cineplex
-        this.idCineplex = idCineplex; //ID of cinema in a cineplex
-        this.listCinemas = new ArrayList<Cinema>();
-    }
+	/**
+	 * Another Constructor for Cineplex but with different parameters
+	 * Allows for a list of cinemas to be set for the Cineplex instead of creating a new list
+	 * @param nameCineplex Name of this cineplex
+	 * @param idCineplex ID of this Cineplex
+	 * @param listCinemas array of all cinemas in this cinemplex
+	 */
+    	public Cineplex(String nameCineplex, int idCineplex, ArrayList<Cinema> listCinemas) {
+        	this.nameCineplex = nameCineplex; 
+        	this.idCineplex = idCineplex; 
+        	this.listCinemas = listCinemas; 
+    	}
 
-    public Cineplex(String nameCineplex, int idCineplex, ArrayList<Cinema> listCinemas) {
-        this.nameCineplex = nameCineplex; //name of cineplex
-        this.idCineplex = idCineplex; //ID of cinema in cineplex
-        this.listCinemas = listCinemas; //array of all the cinemas in a cineplex
-    }
-
-  
-    public String getNameCineplex() { //obtain name of cineplex
-        return nameCineplex;
-    }
+  	/**
+	 * Sets the name of the cineplex
+	 * @param nameCineplex Name of this cineplex
+	 */
+    	public void setNameCineplex(String nameCineplex) { 
+        	this.nameCineplex = nameCineplex; 
+    	}
+	
+	/**
+	 * Gets the name of the cineplex
+	 * @return nameCineplex Name of this cineplex
+	 */
+    	public String getNameCineplex() { 
+        	return nameCineplex;
+    	}	
     
-    public void setNameCineplex(String nameCineplex) { //create name of cineplex 
-        this.nameCineplex = nameCineplex; 
-    }
-
-    public int getIDCineplex() { //obtain id of cineplex
-        return idCineplex;
-    }
+	
+	/**
+	 * Gets the id of the cineplex
+	 * @return idCineplex ID of this cineplex
+	 */
+    	public int getIDCineplex() { 
+        	return idCineplex;
+   	 }
     
-  
-    public ArrayList<Cinema> getListCinemas() { //get list of cinemas in cineplex
-        return listCinemas;
-    }
+  	/**
+	 * Gets list of cinemas in this cineplex
+	 * @return listCinemas list of cinemas in this Cineplex
+	 */
+    	public ArrayList<Cinema> getListCinemas() { //get list of cinemas in cineplex
+        	return listCinemas;
+   	}	
     
- 
-    public void addCinema(Cinema cinema) {
-    	listCinemas.add(cinema); //add cinema to cineplex
-    }
+ 	/**
+	 * Add a Cinema to the list of cinemas in this Cineplex
+	 * @param cinema 
+	 */
+    	public void addCinema(Cinema cinema) {
+    		listCinemas.add(cinema); //add cinema to cineplex
+    	}
 
 }
