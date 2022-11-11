7ddc0b2cd103fa23ae8a65232ef284b48114e5d7
Hillarykwok <104241989+Hillarykwok@users.noreply.github.com>
2022-11-12 02:19:50 +0800

    Update Cinema.java

--- Moblima/src/MOBLIMA/Cinema.java
+++ Moblima/src/MOBLIMA/Cinema.java
@@ -2,18 +2,43 @@ package MOBLIMA;
 //import necessary files;
 import java.util.ArrayList;
 
+/**
+ This cinema class is to create cinemas in cineplexes and to initialise the shows
+ */
 public class Cinema {
 
 	//declaration of the variables;
-    
+    /**
+     * Cinema ID
+     */
     private int idOfCinema;
+    /**
+     * Category of cinema
+     */
     private String cinemaCategory;
+    /**
+     * Name of the cinema
+     */
     private String nameOfCinema;
+    /**
+     * Code of cinema
+     */
     private String cinemaCode;
     
     //array of the shows;
+    /**
+     * Array for the shows in cinema
+     */
     private ArrayList<Show> listOfShows;
     
+	
+    /**
+     * Constructor default
+     * @param nameOfCinema This customers' cinema name
+     * @param cinemaCategory This customers' cinema category
+     * @param cinemaCode This customers' cinema code
+     * @param idOfCinema This customers' cinema ID
+     */	
     public Cinema(String nameOfCinema, String cinemaCategory, String cinemaCode, int idOfCinema) {
         this.nameOfCinema = nameOfCinema;
         this.idOfCinema = idOfCinema;
@@ -24,27 +49,43 @@ public class Cinema {
     }
 
     //get functions;
+    /**
+     * Get the cinema ID
+     * @return cinema ID
+     */
     public int getCinemaID() {
         return idOfCinema;
         
     }
-    
+    /**
+     * Get the cinema name
+     * @return cinema name
+     */
     public String getCinemaName() {
         return nameOfCinema;
         
     }
-
+    /**
+     * Get the cinema category
+     * @return cinema category
+     */
     public String getCinemaCategory() {
         return cinemaCategory;
         
     }
-    
+    /**
+     * Get the cinema code
+     * @return cinema code
+     */
     public String getCinemaCode() {
         return cinemaCode;
         
     }
 
     //display the shows;
+    /**
+     * @return the array of shows present in cinema
+     */
     public ArrayList<Show> getCinemaShows(){
     	return listOfShows;
     }
@@ -52,6 +93,10 @@ public class Cinema {
     
     
     //adding a show;
+    /**
+     * Add a show to cinema
+     * @param s add show to cinema
+     */
     public void addaShow(Show s) {
     	listOfShows.add(s);
     	
