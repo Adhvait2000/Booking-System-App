b55dcfa5c041222a21fcdb22afa771a9ae29505e
pareenakaur <pareenakaur72@gmail.com>
2022-11-11 14:55:05 +0800

    Generated Javadoc

--- Moblima/src/MOBLIMA/PublicHolidayIO.java
+++ Moblima/src/MOBLIMA/PublicHolidayIO.java
@@ -2,10 +2,23 @@ package MOBLIMA;
 
 import java.io.*;
 
+/**
+  Class created to obtain and modify dates of public holidays
+  The dates are retrieved from a data file and modifications are written directly to the file
+  @version 1.0
+  @since 2022-11-02
+ */
+
 public class PublicHolidayIO {
 
-	public String[] readPublicHolidays() throws IOException, Exception {
-
+	/**
+	 * Read dates of public holidays 
+	 * Data is obtained from a text file retrieved from a particular directory
+	 * @throws IOException
+	 * @throws Exception 
+	 * @return public holiday date if reading is successful, or an empty string if there are no more dates or if an exception error has occurred
+	 */
+	public String[] readPublicHolidays() throws IOException,Exception {
 		FileReader text_file;
 		
 		if(System.getProperty("os.name").startsWith("Windows"))
@@ -28,6 +41,14 @@ public class PublicHolidayIO {
 		}
 
 	}
+	
+	/**
+	 * Write dates of public holidays 
+	 * Data is written directly to the text file 
+	 * @throws IOException
+	 * @throws Exception 
+	 * @param publicHolidays String containing one or more dates of publicHolidays
+	 */
 
 	public void writePublicHoliday(String[] publicHolidays) throws IOException, Exception {
 
