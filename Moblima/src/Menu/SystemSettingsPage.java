da2aa8bdffc4d3f1bd0ebeafc8a042891daad341
ArunEzekiel016 <arun016@e.ntu.edu.sg>
2022-11-13 02:51:27 +0800

    Added Create Public holiday menu

--- Moblima/src/Menu/SystemSettingsPage.java
+++ Moblima/src/Menu/SystemSettingsPage.java
@@ -27,6 +27,8 @@ public class SystemSettingsPage extends MenuPage {
 			goNext = true;
 			break;
 		case HOLIDAYS:
+			nextPage = new CreateRemoveUpdatePublicHoliday();
+			goNext = true;
 			break;
 		default:
 			this.endMenu = true;
